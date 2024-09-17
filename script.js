ef0802958aea21d9c0744511aa45f8a8c82a157e
Manmoth <129261117+manmoth-24@users.noreply.github.com>
2024-09-17 16:59:18 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var displayElem = document.getElementById("display")
+var leadLink = document.getElementById("leadLink")
+
+var opacityRate = 0.1
+var chengingTime = 1
+function Display(){
+    if (displayElem.style.opacity != "1"){
+        displayElem.style.opacity = Number(displayElem.style.opacity) + opacityRate;
+    }else{
+        leadLink.style.display = "block"
+    }
+    setTimeout(Display, 100);
+}
+Display()
